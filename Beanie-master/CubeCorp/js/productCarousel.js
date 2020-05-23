143aaa4f3334c1580074f72dbf93f3bfd8547873
justbeanie <contact@beaniebot.me>
2020-05-23 19:04:27 -0400

    add cubecorp

---
+++ Beanie-master/CubeCorp/js/productCarousel.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.product-logos').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 2
+            }
+        }]
+    });
+});
